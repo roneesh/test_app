b914a2bfd5369b8f30d248e57535165834439fad
Roneesh Vashisht <rvashis@searshc.com>
2015-08-14 14:41:30 -0500

    some POSTing of new applications working, but needs to be massaged into JSON

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var express = require('express'),
 	app = express(),
-    server = app.listen(3000, developmentStartup);
+    server = app.listen(3000, developmentStartup),
+    bodyParser = require('body-parser');
 
-var fs = require('fs');
+var fs = require('fs'),
+	applications = [];
+
+// "Database" of applications
+fs.readFile('applications.json', {encoding: 'utf8'}, readApplicationsToArray);
+
+function readApplicationsToArray(error, data) {
+	if (error) {
+		throw error;
+	}
+
+	JSON.parse(data).forEach(function(application) {
+		applications.push(application);
+	});
+}
 
 
 // Express Setup
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', './views');
 app.set('view engine', 'jade');
 
@@ -26,11 +43,19 @@ function showApplication(request, response) {
 }
 
 function indexApplication(request, response) {
-    response.render('index')
+    response.render('index', {applications: applications});
 }
 
 function createApplication(request, response) {
-
+	console.log('POST applications hit')
+	console.log(request.body)
+	var new_application = {
+		'name' 		 : request.body.name,
+		'age'  		 : request.body.age,
+		'profession' : request.body.profession
+	}
+	fs.appendFile('applications.json', new_application);
+	response.render('index', {applications: applications, thanks: 'THANKZ'});
 }
 
 // Development Server Startup
