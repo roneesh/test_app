39fed7f2de549f1bdacfe74515a8d3c95ff62e9f
Roneesh <roneesh@gmail.com>
2015-08-14 21:11:14 -0500

    adding server side validation of new records

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ var express = require('express'),
     fs = require('fs'),
     _ = require('underscore');
 
-// Express Setup
+
+// *************
+// Express setup
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+
+// ******
 // Routes
 app.get('/', newApplication);
 app.get('/applications/new', newApplication);
@@ -35,22 +39,36 @@ function indexApplication(request, response) {
 
 function createApplication(request, response) {
 	var new_application = {
-		'name' 		 : request.body.name,
-		'age'  		 : request.body.age,
-		'profession' : request.body.profession
+		'name' 		  : request.body.name,
+		'age'  		  : request.body.age,
+		'profession'  : request.body.profession,
 	}
-	fs.appendFile('applications.json', new_application);
-	response.render('index', {applications: applications, thanks: 'THANKZ'});
+
+	if (recordIsValid(new_application)) {
+		_.extend(new_application, {
+			'application_id' 	   : new Date(),
+			'application_reviewed' : false,
+			'application_approved' : false
+		})
+		createApplicationRecord(new_application);
+		response.render('index', {applications: readDB(), thanks: 'THANKZ'});
+	} else {
+		response.render('index', {applications: readDB(), message: 'We are sorry, but your application could not be saved at this time, please try again.'})
+	}
 }
 
-// Development Server Startup
-function developmentStartup() {
-    console.log('Server is running on port ' + server.address().port);
+
+// ****************************************
+// "Database" of applications via JSON file
+
+function recordIsValid(application) {
+	return /^[a-zA-Z][a-zA-Z\s]{2,20}/.test(application.name) &&
+		   /^[1-9][0-9]$/.test(application.age) &&
+		   /^[a-zA-Z][a-zA-Z\s]{2,50}/.test(application.profession);
 }
 
-// "DATABASE" of applications via JSON file
 function readDB() {
-	return JSON.parse(fs.readFileSync('applications.json', {encoding: 'utf8'}));
+	return JSON.parse(fs.readFileSync('applications2.json', {encoding: 'utf8'}));
 }
 
 function getApplications() {
@@ -64,7 +82,14 @@ function getApplication(application_id) {
 function createApplicationRecord(new_record) {
 	var existing_db = readDB();
 	existing_db.push(new_record);
-	// return fs.writeFileSync('applications.json', existing_db);
+	return fs.writeFileSync('applications2.json', JSON.stringify(existing_db, null, 4));
 }
 
-createApplicationRecord({'test_record':'test_value'});
+
+// **************************
+// Development server startup
+
+function developmentStartup() {
+    console.log('Server is running on port ' + server.address().port);
+}
+
