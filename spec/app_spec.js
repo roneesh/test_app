668f679d78959ab585e601b27254c8c2d6d84d13
Roneesh Vashisht <rvashis@searshc.com>
2015-08-20 11:57:33 -0500

    second passing test for server side validation function

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -2,12 +2,24 @@ var index = require('../index.js');
 
 describe('recordIsValid', function() {
 	var valid_object = {
-		'name' : 'john smith',
-		'age' : '35',
-		'profession' : 'programmer'
-	}
-	it('returns true for a passing object', function() {
+			'name' 		 : 'john smith',
+			'age' 		 : '35',
+			'profession' : 'programmer'
+		},
+		invalid_object = {
+			'name' 		 : '123456',
+			'age'  		 : 'john smith',
+			'profession' : '12346'
+		};
+
+	it('returns true for a valid object', function() {
 		var validity = index.recordIsValid(valid_object);
 		expect(validity).toEqual(true);
 	});
+
+	it('returns false for an invalid object', function() {
+		var validity = index.recordIsValid(invalid_object);
+		expect(validity).toEqual(false);
+	});
+
 })
